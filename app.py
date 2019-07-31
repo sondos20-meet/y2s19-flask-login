dc613753606a44175d61d2a6de11dcc0d0f78cc9
myname21-meet <myname21@meet.mit.edu>
2019-07-31 04:27:58 -0400

    something

--- app.py
+++ app.py
@@ -41,5 +41,6 @@ def logout():
 
 
 
+
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -6,13 +6,15 @@ from passlib.apps import custom_app_context as pwd_security
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    #what else does a user account need?
-    password_hash = Column(String)
+  
 
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password_hash = Column(String)
+   
     def hash_password(self, password):
-        self.password_hash = pwd_security.encrypt(password)
+       self.password_hash = pwd_security.encrypt(password)
     def verify_password(self, password):
-        return pwd_security.verify(password, self.password_hash)
+       return pwd_security.verify(password,self.password_hash)
